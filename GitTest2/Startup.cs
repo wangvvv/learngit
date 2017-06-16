7a486399b3db314912c08bfbc9d5f4d2b3275633
wangv <455257757@qq.com>
2017-06-16 10:06:02 +0800

    用作git的test

---
+++ GitTest2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitTest2.Startup))]
+namespace GitTest2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
